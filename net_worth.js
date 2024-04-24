e52556e4c2353d4c37c2d30b2ddab7995698d97a
Antonio <asoriano@appdev.yearup.org>
2024-04-24 11:08:02 -0500

    completed net_worth.js

---
+++ net_worth.js
@@ -0,0 +1,14 @@
+let cash = 3.50;
+let checkings = 2000;
+let savings = 0.99;
+
+let assets = (cash + checkings + savings);
+
+let carLoan = 10000;
+let creditCard = 1500;
+
+let debts = (carLoan + creditCard);
+
+let netWorth = (assets - debts);
+
+console.log("My net worth is " + netWorth);
