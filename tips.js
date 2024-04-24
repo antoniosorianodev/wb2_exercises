aca6df563c7c2ba2114f7e6c1d19b819f3fcabf5
Antonio <asoriano@appdev.yearup.org>
2024-04-24 10:56:21 -0500

    completed tips.js

---
+++ tips.js
@@ -0,0 +1,11 @@
+let foodCost = 79.25;
+let tax = 6.52;
+let tip = 12.00;
+
+// do our calculations for the variables above
+let totalDue = foodCost + tax + tip;
+
+// output the results to the console
+console.log("The Food Cost is $" + foodCost + " and the tax is $" + tax);
+console.log("The tip is $" + tip);
+console.log("The total due is $" + totalDue);
