d3ee23d2447fa1e1546ab25c11b78607d6b51af3
Antonio <asoriano@appdev.yearup.org>
2024-04-25 09:55:03 -0500

    completed distance.js

---
+++ distance.js
@@ -0,0 +1,6 @@
+let object1 = [0, 0];
+let object2 = [3, 4];
+
+let distance = (((object1[0] - object2[0]) ** 2) + ((object1[1] - object2[1]) ** 2)) ** 0.5;
+
+console.log("The distance between object 1 and object 2 is " + distance);
