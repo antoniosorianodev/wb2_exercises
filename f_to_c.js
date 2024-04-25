2005b41d1ef2c3c8c9e54c7248e87cb59f8e0700
Antonio <asoriano@appdev.yearup.org>
2024-04-25 09:16:58 -0500

    completed f_to_c.js

---
+++ f_to_c.js
@@ -0,0 +1,5 @@
+let fahrenheit = 100;
+
+let celsius = (fahrenheit - 32) * (5 / 9);
+
+console.log("A temperature of " + fahrenheit + " degrees fahrenheit is equivalent to " + celsius + " degrees celsius");
