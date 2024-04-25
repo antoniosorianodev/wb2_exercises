61427a53089837b4dceea8d4f6f96f39d33a7f8f
Antonio <asoriano@appdev.yearup.org>
2024-04-25 11:48:05 -0500

    completed conversion_tests.js

---
+++ conversion_tests.js
@@ -0,0 +1,9 @@
+let a = "   101.1   ";
+let b = "55";
+let c = "402 Stevens";
+let d = "Number 5   "
+
+console.log("parseInt: " + parseInt(a) + ", " + parseInt(b) + ", " + parseInt(c) + ", " + parseInt(d));
+console.log("parseFloat: " + parseFloat(a) + ", " + parseFloat(b) + ", " + parseFloat(c) + ", " + parseFloat(d));
+console.log("Number: " + Number(a) + ", " + Number(b) + ", " + Number(c) + ", " + Number(d));
+console.log("Unary operator (+): " + +a + ", " + +b + ", " + +c + ", " + +d);
