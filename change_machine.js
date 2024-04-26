70e6250768bb6b181d8191e0743aaa5e651c3c4c
Antonio <asoriano@appdev.yearup.org>
2024-04-26 11:48:31 -0500

    cut down on redundant lines and added comment to change_machine.js

--- change_machine.js
+++ change_machine.js
@@ -1,33 +1,26 @@
 let dollarValue = 47;
-let change = [];
+// change = [twenties, tens, fives, ones]
+let change = [0, 0, 0, 0];
 
 if ((dollarValue / 20) >= 1) {
     let twenties = Math.floor(dollarValue / 20);
     change[0] = twenties;
     dollarValue -= (twenties * 20);
-} else {
-    change[0] = 0;
 }
 if ((dollarValue / 10) >= 1) {
     let tens = Math.floor(dollarValue / 10);
     change[1] = tens;
     dollarValue -= (tens * 10);
-} else {
-    change[1] = 0;
 }
 if ((dollarValue / 5) >= 1) {
     let fives = Math.floor(dollarValue / 5);
     change[2] = fives;
     dollarValue -= (fives * 5);
-} else {
-    change[2] = 0;
 }
 if ((dollarValue / 1) >= 1) {
-    let ones = Math.floor(dollarValue / 1);
+    let ones = Math.floor(dollarValue);
     change[3] = ones;
-    dollarValue -= (ones * 1);
-} else {
-    change[3] = 0;
+    dollarValue -= (ones);
 }
 
 console.log(change);
